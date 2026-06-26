1e3d6520b0ac5c488ff74b7cafe190e3d9f77fa5
Lizabeth Arum <lizarum@gmail.com>
2026-06-26 16:26:40 -0400

    MakeCode + days-default: add Launch Sequence starter code (Day 3 turning + new tile)

--- public/days-default.js
+++ public/days-default.js
@@ -290,7 +290,7 @@ const DAYS_DEFAULT = [
         "worksheet": [{url:"https://drive.google.com/file/d/1NCR1-2-ASqbQNvBeXOKvx67YOQHpbv1y/view?usp=drive_link",label:"Arc Approximation: Introducing Turning Worksheet"},
           {url: "/resources/launch-sequence-help.html", label: "Launch Sequence Help"}],
      
-       "makecode":"https://makecode.microbit.org/S79542-10060-01167-82601",
+       "makecode":[{"url": "https://makecode.microbit.org/S79542-10060-01167-82601", "label": "MakeCode Project"},{"url": "https://makecode.microbit.org/S61805-34921-25084-54341", "label": "Launch Sequence Starter Code"}],
          "exemplar": "https://velocity-arena-gold.vercel.app/resources/teacher-guides/exemplars/VA_Exemplar_W1_D3_Arc_Approximation_Turning.html",
         "quickCard": "https://velocity-arena-gold.vercel.app/resources/quick-cards/activity-card-day03-03-arc-approximation.html"
       },
